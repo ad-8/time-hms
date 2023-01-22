ab60451a9217b4ca530543afe954eb01e851cc6c
Alex D <alex8d@pm.me>
2023-01-22 23:29:41 +0100

    port Go version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let foo = TimeHMS::new(12345);
+
+    println!("{:?}", foo);
+}
+
+#[derive(Debug)]
+struct TimeHMS {
+    h: i32,
+    m: i32,
+    s: i32,
+}
+
+impl TimeHMS {
+    fn new(seconds: i32) -> TimeHMS {
+        let (m, s) = divmod(seconds, 60);
+        let (h, m) = divmod(m, 60);
+        TimeHMS { h, m, s }
+    }
+}
+
+fn divmod(x: i32, y: i32) -> (i32, i32) {
+    let quotient = x / y;
+    let remainder = x % y;
+    (quotient, remainder)
 }
