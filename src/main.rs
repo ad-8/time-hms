f3aaaad93e390520e7c5fb33dc587b46741ab3d8
Alex D <alex8d@pm.me>
2023-01-22 23:33:35 +0100

    add docstrings

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     println!("{:?}", foo);
 }
 
+/// SimpleTime represents a duration in hours, minutes and seconds.
 #[derive(Debug)]
 struct TimeHMS {
     h: i32,
@@ -12,13 +13,17 @@ struct TimeHMS {
 }
 
 impl TimeHMS {
+    /// Converts a duration from a representation in seconds
+    /// into a representation in hours, minutes and seconds.
     fn new(seconds: i32) -> TimeHMS {
+        // TODO return Result (err if seconds < 0)
         let (m, s) = divmod(seconds, 60);
         let (h, m) = divmod(m, 60);
         TimeHMS { h, m, s }
     }
 }
 
+/// Takes two numbers as arguments and returns their quotient and remainder when using integer division.
 fn divmod(x: i32, y: i32) -> (i32, i32) {
     let quotient = x / y;
     let remainder = x % y;
