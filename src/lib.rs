567d390ea6fe453ed7d1d54aaf05bda32a86d860
Alex D <alex8d@pm.me>
2023-01-31 10:29:10 +0100

    refactor: use u64 to simplify implementation and usage

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::fmt;
 /// ```
 /// use time_hms::TimeHMS;
 ///
-/// let t = TimeHMS::new(3723).unwrap();
+/// let t = TimeHMS::new(3723);
 ///
 /// // 3723 seconds equal 1 hour, 2 minutes and 3 seconds
 /// assert_eq!(t.h(), 1);
@@ -17,37 +17,33 @@ use std::fmt;
 ///```
 #[derive(Debug, PartialEq)]
 pub struct TimeHMS {
-    h: i64,
-    m: i64,
-    s: i64,
+    h: u64,
+    m: u64,
+    s: u64,
 }
 
 impl TimeHMS {
     /// Converts a duration from a representation in seconds
     /// into a representation in hours, minutes and seconds.
-    pub fn new(seconds: i64) -> Result<TimeHMS, &'static str> {
-        if seconds < 0 {
-            return Err("seconds must be >= 0");
-        }
-
+    pub fn new(seconds: u64) -> TimeHMS {
         let (m, s) = divmod(seconds, 60);
         let (h, m) = divmod(m, 60);
 
-        Ok(TimeHMS { h, m, s })
+        TimeHMS { h, m, s }
     }
 
     /// Returns the hour part of the duration.
-    pub fn h(&self) -> i64 {
+    pub fn h(&self) -> u64 {
         self.h
     }
 
     /// Returns the minute part of the duration.
-    pub fn m(&self) -> i64 {
+    pub fn m(&self) -> u64 {
         self.m
     }
 
     /// Returns the seconds part of the duration.
-    pub fn s(&self) -> i64 {
+    pub fn s(&self) -> u64 {
         self.s
     }
 }
@@ -59,7 +55,7 @@ impl fmt::Display for TimeHMS {
 }
 
 /// Takes two numbers as arguments and returns their quotient and remainder when using integer division.
-fn divmod(x: i64, y: i64) -> (i64, i64) {
+fn divmod(x: u64, y: u64) -> (u64, u64) {
     let quotient = x / y;
     let remainder = x % y;
     (quotient, remainder)
@@ -76,31 +72,13 @@ mod tests {
             (10, 3, (3, 1)),
             (13, 5, (2, 3)),
             (0, 5, (0, 0)),
-            (-8, 3, (-2, -2)),
-            (8, -3, (-2, 2)),
-            (-8, -3, (2, -2)),
-            (i64::MIN, 1, (i64::MIN, 0)),
-            (i64::MAX, 1, (i64::MAX, 0)),
-            (i64::MIN, i64::MIN, (1, 0)),
-            (i64::MAX, i64::MAX, (1, 0)),
+            (u64::MAX, 1, (u64::MAX, 0)),
+            (u64::MAX, u64::MAX, (1, 0)),
         ];
 
         for (x, y, expected) in test_cases {
             assert_eq!(divmod(x, y), expected);
         }
-    }
-
-    #[test]
-    fn invalid_arg() {
-        let t = TimeHMS::new(-1);
-        assert!(t.is_err());
-    }
-
-    #[test]
-    fn test_err_msg() {
-        let t = TimeHMS::new(-1);
-        let err = t.unwrap_err();
-        assert_eq!(err.to_string(), "seconds must be >= 0".to_string());
     }
 
     #[test]
@@ -112,7 +90,7 @@ mod tests {
         ];
 
         for (input, expected) in test_cases {
-            let t = TimeHMS::new(input).unwrap();
+            let t = TimeHMS::new(input);
             let (h, m, s) = expected;
             assert_eq!(t.h, h);
             assert_eq!(t.m, m);
@@ -122,15 +100,15 @@ mod tests {
 
     #[test]
     fn equal() {
-        let t1 = TimeHMS::new(3661).unwrap();
-        let t2 = TimeHMS::new(3661).unwrap();
+        let t1 = TimeHMS::new(3661);
+        let t2 = TimeHMS::new(3661);
         assert_eq!(t1, t2);
     }
 
     #[test]
     fn not_equal() {
-        let t1 = TimeHMS::new(3661).unwrap();
-        let t2 = TimeHMS::new(3662).unwrap();
+        let t1 = TimeHMS::new(3661);
+        let t2 = TimeHMS::new(3662);
         assert_ne!(t1, t2);
     }
 }
